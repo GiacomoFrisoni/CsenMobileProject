d2334f07375bfb44acbffcad33ee0572ac634cc4
Marcin Pabich <martinoqom@gmail.com>
2019-09-27 13:57:26 +0200

    Quick Refactor of MainActivity

--- CsenPoomsaeScore/CsenPoomsaeScore/app/src/main/java/it/frisoni/pabich/csenpoomsaescore/MainActivity.java
+++ CsenPoomsaeScore/CsenPoomsaeScore/app/src/main/java/it/frisoni/pabich/csenpoomsaescore/MainActivity.java
@@ -41,19 +41,9 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnMe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-         * Inizializza l'oggetto per la gestione delle SharedPreferences.
-         */
         appPrefs = new AppPreferences(MainActivity.this);
-
-        /*
-         * Inizializza l'oggetto per la gestione della vibrazione.
-         */
         VibrationHandler.getHandler().initialize(MainActivity.this);
 
-        /*
-         * Salva nelle SharedPreferences la password criptata al primo avvio dell'applicazione.
-         */
         if (appPrefs.getFirstTimeKey()) {
             try {
                 byte[] pw_bytes = new byte[]{-115, -46, -60, 14, 56, 115, -88, 112, -84, -118, 116, 103, 100, 33, -128, 74};
@@ -64,113 +54,44 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnMe
             }
         }
 
-        /*
-         * Popolazione del layout con l'istanza di MenuFragment.
-         */
         addFragment(MenuFragment.newInstance(), false);
+    }
+
+
+    // ----------------------------------------------------------------------------------- MAIN MENU
+
+    @Override
+    public void onStartNewEvaluation() {
+        replaceFragment(AccuracyFragment.newInstance(), true);
+        VibrationHandler.getHandler().vibrate();
     }
 
     @Override
     public void onScoresClick() {
-        /*
-         * Replace del fragment nel layout con l'istanza di ScoresFragment.
-         */
         replaceFragment(ScoresFragment.newInstance(), true);
         VibrationHandler.getHandler().vibrate();
     }
 
     @Override
     public void onSettingsClick() {
-        /*
-         * Replace del fragment nel layout con l'istanza di SettingsFragment.
-         */
         replaceFragment(SettingsFragment.newInstance(), true);
         VibrationHandler.getHandler().vibrate();
     }
 
+
+
     @Override
     public void onMenuClick() {
-        /*
-         * Replace del fragment nel layout con l'istanza di MenuFragment.
-         * Tornano al menù dalla schermata di results, lo stack viene ripulito per impedire
-         * il back.
-         */
-        FragmentManager manager = getSupportFragmentManager();
+        final FragmentManager manager = getSupportFragmentManager();
         for (int i = 0; i < manager.getBackStackEntryCount(); i++) {
             manager.popBackStack();
         }
-        //replaceFragment(MenuFragment.newInstance(), false);
+
         VibrationHandler.getHandler().vibrate();
-    }
-
-    @Override
-    public void onStartNewEvaluation() {
-        replaceFragment(AccuracyFragment.newInstance(), true);
-        VibrationHandler.getHandler().vibrate();
-        /*
-         * Replace del fragment nel layout con l'istanza di AccuracyFragment.
-
-        if (ConnectionHelper.isConnectionAvaiable(this)) {
-            if (!ConnectionHelper.isConnectionEstabished()) {
-                new android.app.AlertDialog.Builder(this)
-                        .setTitle(getString(R.string.attention))
-                        .setMessage(getString(R.string.connection_avaiable_message))
-                        .setIconAttribute(android.R.attr.alertDialogIcon)
-                        .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                                Toast.makeText(getBaseContext(), getString(R.string.connection_config_server), Toast.LENGTH_LONG);
-                            }
-                        })
-                        .setNegativeButton(getString(R.string.connection_continue_offline), new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-
-                                replaceFragment(AccuracyFragment.newInstance(), true);
-                                VibrationHandler.getHandler().vibrate();
-                            }
-                        })
-                        .show();
-            } else {
-                replaceFragment(AccuracyFragment.newInstance(), true);
-                VibrationHandler.getHandler().vibrate();
-            }
-
-        } else {
-
-            new android.app.AlertDialog.Builder(this)
-                    .setTitle(getString(R.string.attention))
-                    .setMessage(getString(R.string.internet_avaiable_message))
-                    .setIconAttribute(android.R.attr.alertDialogIcon)
-                    .setPositiveButton(getString(R.string.internet_wait), new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                            Toast.makeText(getBaseContext(), getString(R.string.internet_wait), Toast.LENGTH_LONG);
-                        }
-                    })
-                    .setNegativeButton(getString(R.string.connection_continue_offline), new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-
-                            replaceFragment(AccuracyFragment.newInstance(), true);
-                            VibrationHandler.getHandler().vibrate();
-                        }
-                    })
-                    .show();
-        }*/
-
-
     }
 
     @Override
     public void onPresentationClick(float accuracyPoints) {
-        /*
-         * Aggiunta di PresentationFragment.
-         */
         this.accuracyPoints = accuracyPoints;
         addFragment(PresentationFragment.newInstance(), true);
         VibrationHandler.getHandler().vibrate();
@@ -184,10 +105,6 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnMe
 
     @Override
     public void onResultsClick(float presentationPoints, boolean isReadOnly) {
-        /*
-         * Aggiunta di ResultsFragment.
-         */
-
         addFragment(ResultsFragment.newInstance(this.accuracyPoints, presentationPoints, isReadOnly), true);
         VibrationHandler.getHandler().vibrate();
     }
